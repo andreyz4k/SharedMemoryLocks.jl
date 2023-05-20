36f7f588036efe566c09111151fb9fceb5894602
Andrey Zakharevich <andreyzakharevich@gmail.com>
2023-05-20 19:43:02 +0300

    Create a class for a lock shared between local processes

--- src/SharedMemoryLocks.jl
+++ src/SharedMemoryLocks.jl
@@ -1,5 +1,49 @@
 module SharedMemoryLocks
 
-# Write your package code here.
+using Distributed
+using SharedArrays
+struct SharedMemoryLock <: Base.AbstractLock
+    owned::SharedArray{Int64,1}
+    SharedMemoryLock(pids::Vector{Int64} = Int64[]) = new(SharedArray{Int64,1}(1, init = 0, pids = pids))
+    SharedMemoryLock(owned::SharedArray{Int64,1}) = new(owned)
+end
+export SharedMemoryLock
+
+using Core.Intrinsics: atomic_pointerreplace
+
+function Base.lock(l::SharedMemoryLock)
+    while true
+        if @inline trylock(l)
+            return
+        end
+        ccall(:jl_cpu_pause, Cvoid, ())
+        # Temporary solution before we have gc transition support in codegen.
+        ccall(:jl_gc_safepoint, Cvoid, ())
+    end
+end
+
+function Base.trylock(l::SharedMemoryLock)
+    res = atomic_pointerreplace(pointer(l.owned, 1), 0, myid(), :sequentially_consistent, :sequentially_consistent)
+    return res.success
+end
+
+function Base.unlock(l::SharedMemoryLock)
+    res = atomic_pointerreplace(pointer(l.owned, 1), myid(), 0, :sequentially_consistent, :sequentially_consistent)
+    if !res.success
+        error("can't unlock a lock that is not owned by the current process")
+    end
+    GC.enable_finalizers()
+    ccall(:jl_cpu_wake, Cvoid, ())
+    return
+end
+
+function Base.islocked(l::SharedMemoryLock)
+    return l.owned[1] != 0
+end
+
+using Serialization
+Serialization.serialize(s::AbstractSerializer, l::SharedMemoryLock) = Serialization.serialize_any(s, l)
+Serialization.deserialize(s::AbstractSerializer, t::Type{SharedMemoryLock}) =
+    @invoke Serialization.deserialize(s::AbstractSerializer, t::DataType)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,40 @@
-using SharedMemoryLocks
 using Test
+using Distributed
+using SharedArrays
+using SharedMemoryLocks
 
 @testset "SharedMemoryLocks.jl" begin
-    # Write your tests here.
+    @testset "Basic" begin
+        l = SharedMemoryLocks.SharedMemoryLock()
+        @test !islocked(l)
+        lock(l)
+        @test islocked(l)
+        unlock(l)
+        @test !islocked(l)
+    end
+
+    @testset "Multiprocess" begin
+        pids = addprocs(2)
+        @everywhere pids using SharedMemoryLocks
+
+        push!(pids, myid())
+        l = SharedMemoryLock(pids)
+        arr = SharedArray{Int}(1, init = 0, pids = pids)
+
+        function calc(l, arr)
+            for i in 1:100
+                lock(l)
+                v = arr[1]
+                sleep(0.000000001)
+                arr[1] = v + 1
+                unlock(l)
+            end
+        end
+
+        f1 = @spawnat pids[1] calc(l, arr)
+        f2 = @spawnat pids[2] calc(l, arr)
+        fetch(f1)
+        fetch(f2)
+        @test arr[1] == 200
+    end
 end
